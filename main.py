631b34159938c5c338cd2eb62cb730f821aa2b1d
zdwhite <zdwhitepdx@gmail.com>
2020-05-05 16:49:41 -0700

    added code to read in poker.txt file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 def main():
+    file_name = 'poker.txt'
+    with open(file_name,'r') as games:
+        hands = games.readall()
+        print(hands)
 
     # run the functions defined above
-
 if __name__ == "__main__":
 	main()
