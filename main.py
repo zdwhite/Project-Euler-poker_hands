d2b26eeaf6e29b493a8ca6afd1711062d3d8b4d1
zdwhite <zdwhitepdx@gmail.com>
2020-05-05 17:12:01 -0700

    Added parse_games function to return a list of all the games in the poker.txt file

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 def main():
     file_name = 'poker.txt'
     with open(file_name,'r') as games:
-        hands = games.readall()
-        print(hands)
+        #hands = games.read()
+        #print("Player 1:{}\nPlayer 2:{}".format(hands[0:14], hands[15:29]))
+        #hands = games.readline()
+        #print(hands)
+        data = []
+        while games.readline():
+            data.append(games.readline()[:-1])
+        print(data[0])
 
     # run the functions defined above
 if __name__ == "__main__":
