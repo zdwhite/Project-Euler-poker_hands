5cb88ddd745d87278809213e2a88d31bfc6953db
zdwhite <zdwhitepdx@gmail.com>
2020-05-05 19:05:09 -0700

    Added function to return the highest card in a hand

--- main.py
+++ main.py
@@ -23,11 +23,30 @@ def parse_players(games):
         player_2.append(game[15:])
     return player_1, player_2
 
+def high_card(hand):
+    """Determine the high card in a hand of poker"""
+    card_rank = ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+    cards_in_hand = hand.split(" ")
+    high_rank=[0]
+    high_card=[0]
+    for card in cards_in_hand:
+        rank_card = card_rank.index(card[0])+1
+        print("Highest card rank: {}\n Current card rank: {} ".format(high_rank[0],rank_card))
+        if  rank_card > high_rank[0]:
+            high_rank[0] = card_rank.index(card[0])
+            high_card[0] = card
+
+    print(high_rank, " ",high_card, " " ,hand)
+
 def main():
     file_name = 'poker.txt'
+    card_rank = ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"]
     games = parse_games(file_name)
     player_1,player_2 = parse_players(games)
-    print(player_1[:5],"\n",player_2[:5])
+    #print(player_1[:5],"\n",player_2[:5])
+
+    print(player_1[0].split(" "))
+    high_card(player_1[0])
 
 
     # run the functions defined above
